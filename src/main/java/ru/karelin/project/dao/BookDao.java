ce316ee915f5b39ed319c8bae50dea7bf47e5974
Vlad <jeywer2747@gmail.com>
2023-05-11 23:14:18 +0300

    start making DAO

--- src/main/java/ru/karelin/project/dao/BookDao.java
+++ src/main/java/ru/karelin/project/dao/BookDao.java
@@ -1,8 +1,12 @@
 package ru.karelin.project.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import ru.karelin.project.models.Person;
+
+import java.util.List;
 
 @Component
 public class BookDao {
@@ -13,5 +17,11 @@ public class BookDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    public List<Person> index(){
+        String SQL = "SELECT * FROM Person";
+        List<Person> people = jdbcTemplate.query(SQL, new BeanPropertyRowMapper<>());
+
+        return people;
+    }
 
 }
